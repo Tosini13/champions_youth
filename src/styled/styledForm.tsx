891233e37ab2258101bb290e5e9725f545a306fd
jakub.bartosik@mayeryn.com <jbartos13@gmail.com>
2021-03-20 15:45:23 +0100

    inputs color changed

--- src/styled/styledForm.tsx
+++ src/styled/styledForm.tsx
@@ -1,10 +1,16 @@
+import React from "react";
 import styled from "styled-components";
 
 import { Checkbox } from "@material-ui/core";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import { KeyboardDatePicker, KeyboardTimePicker } from "@material-ui/pickers";
+import TextField, { TextFieldProps } from "@material-ui/core/TextField";
+import {
+  KeyboardDatePicker,
+  KeyboardDatePickerProps,
+  KeyboardTimePicker,
+  KeyboardTimePickerProps,
+} from "@material-ui/pickers";
 import FormLabel from "@material-ui/core/FormLabel";
 import FormControl from "@material-ui/core/FormControl";
 import Stepper from "@material-ui/core/Stepper";
@@ -28,18 +34,28 @@ const INPUT_STYLE = `
   input {
     color: ${mainTheme.palette.secondary.main};
     &::before {
-      border-color: ${mainTheme.palette.secondary.dark};
-    }
+      border-color: blue;
+    };
   };
   label {
     color: ${mainTheme.palette.secondary.dark};
   };
+  .MuiInput-underline:before{
+    border-color: ${mainTheme.palette.secondary.dark};;
+  };
 `;
 
-export const TextFieldStyled = styled(TextField)`
+export const TemplateTextFieldStyled = styled(TextField)`
   ${INPUT_STYLE};
   max-width: 250px;
 `;
+
+/* ---------------------------------------- */
+/* TextField */
+/* ---------------------------------------- */
+export const TextFieldStyled = (props: TextFieldProps) => (
+  <TemplateTextFieldStyled color="secondary" {...props} />
+);
 
 const KEYBOARD_PICKER_STYLE = `
   button{
@@ -48,14 +64,26 @@ const KEYBOARD_PICKER_STYLE = `
   ${INPUT_STYLE};
 `;
 
-export const KeyboardDatePickerStyled = styled(KeyboardDatePicker)`
+export const TemplateKeyboardDatePickerStyled = styled(KeyboardDatePicker)`
   ${KEYBOARD_PICKER_STYLE};
 `;
 
-export const KeyboardTimePickerStyled = styled(KeyboardTimePicker)`
+/* ---------------------------------------- */
+/* KeyboardDatePicker */
+/* ---------------------------------------- */
+export const KeyboardDatePickerStyled = (props: KeyboardDatePickerProps) => (
+  <TemplateKeyboardDatePickerStyled color="secondary" {...props} />
+);
+export const TemplateKeyboardTimePickerStyled = styled(KeyboardTimePicker)`
   ${KEYBOARD_PICKER_STYLE};
 `;
 
+/* ---------------------------------------- */
+/* KeyboardTimePicker */
+/* ---------------------------------------- */
+export const KeyboardTimePickerStyled = (props: KeyboardTimePickerProps) => (
+  <TemplateKeyboardTimePickerStyled color="secondary" {...props} />
+);
 export const CheckboxStyled = styled(Checkbox)`
   svg {
     color: ${mainTheme.palette.secondary.dark};
